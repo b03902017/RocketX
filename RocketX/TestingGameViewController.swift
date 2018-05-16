4b5ce109280fe9a9287edb5d543a524088cc638f
許少昀 <serval@willyde-macbook-pro.local>
2018-05-17 00:42:01 +0800

    What I added: 	1. The drone model in art.scnassets; 	2. A .swift file called TestingGameViewController; 	3. A View Controller in main.storyboard. The TestingGameVIewController create a scene with asteroids flying toward the camera, and the model drone stays at origin. The View Controller I added comes with only a SCNView, and I let the app start with this viewController. So if you build and run the app, you are supposed to see what i made. (I wonder if i should create a branch for this, but I don't know how to...)

---
+++ RocketX/TestingGameViewController.swift
@@ -0,0 +1,140 @@
+//
+//  GameViewController.swift
+//  Asteroid_Test
+//
+//  Created by Willy on 2018/5/16.
+//  Copyright © 2018年 Willy. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+import SceneKit
+
+class GameViewController: UIViewController, SCNSceneRendererDelegate {
+
+    
+    // MARK: Properties
+    var gameView: SCNView!
+    var gameScene: SCNScene!
+    var cameraNode: SCNNode!
+    var shipNode: SCNNode!
+    var asteroidCreationTiming: Double = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initView()
+        initScene()
+        initCamera()
+        initShip()
+        //createAsteroid()
+    }
+    
+    // MARK: functions
+    
+    func initView() {
+        gameView = self.view as! SCNView
+        gameView.allowsCameraControl = true
+        gameView.autoenablesDefaultLighting = true
+        gameView.delegate = self
+        
+    }
+    
+    func initScene() {
+        gameScene = SCNScene()
+        gameView.scene = gameScene
+        gameView.scene?.physicsWorld.gravity = SCNVector3(x: 0, y: 0, z: 0)
+        gameView.scene?.physicsWorld.speed = 1
+        gameScene.background.contents = UIColor.darkGray
+        
+    }
+    
+    func initCamera() {
+        cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 10   )
+        gameScene.rootNode.addChildNode(cameraNode)
+    }
+    
+    func initShip() {
+        let shipScene = SCNScene(named: "art.scnassets/Drone.dae")
+        shipNode = shipScene?.rootNode
+        shipNode.position = SCNVector3(x: 0, y: 0, z: 0)
+        shipNode.scale = SCNVector3(x: 15, y: 15, z: 15)
+        gameScene.rootNode.addChildNode(shipNode)
+    }
+    
+    func createAsteroid() {
+        // create the SCNGeometry
+        let asteroidGeometry = SCNCapsule(capRadius: 3, height: 7)
+        // create the SCNNode with SCNGeometry
+        let asteroidNode = SCNNode(geometry: asteroidGeometry)
+        
+        //setting the asteroid spawn position
+        let randomAsteroidPositionX = Float((drand48()-0.5)*6)
+        let randomAsteroidPositionY = Float((drand48()-0.5)*6)
+        asteroidNode.position = SCNVector3(x: randomAsteroidPositionX, y: randomAsteroidPositionY, z: -50)
+        
+        // put the asteroid into the rootNode
+        gameScene.rootNode.addChildNode(asteroidNode)
+        
+        asteroidNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        asteroidNode.physicsBody?.damping = 0
+        //apllying forces and torques
+        let asteroidInitialForce = SCNVector3(x: 0, y: 0, z: 10)
+        asteroidNode.physicsBody?.applyForce(asteroidInitialForce, asImpulse: true)
+        let randomAsteroidTorqueX = Float(drand48()-0.5)
+        let randomAsteroidTorqueY = Float(drand48()-0.5)
+        let randomAsteroidTorqueZ = Float(drand48()-0.5)
+        asteroidNode.physicsBody?.applyTorque(SCNVector4(x: randomAsteroidTorqueX, y: randomAsteroidTorqueY, z: randomAsteroidTorqueZ, w: 5), asImpulse: true)
+        
+        
+        
+    }
+    
+    
+    
+    // MARK: SCNSceneRendererDeligate functions
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        if time > asteroidCreationTiming {
+            createAsteroid()
+            asteroidCreationTiming = time + 4
+            cleanUp()
+        }
+    }
+    
+    // remove the unseeable asteroid behind the camera
+    func cleanUp() {
+        for node in gameScene.rootNode.childNodes {
+            if node.presentation.position.z > 50 {
+                node.removeFromParentNode()
+            }
+        }
+    }
+    
+    
+    
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+}
