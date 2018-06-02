87aeb5e294f82c2d75cbbaab4d4328dfb1c6dc55
許少昀 <serval@willyde-macbook-pro.local>
2018-06-02 15:15:48 +0800

    Improved the ship control, especially around the edge.

--- RocketX/AsteroidGameViewController.swift
+++ RocketX/AsteroidGameViewController.swift
@@ -35,11 +35,15 @@ class AsteroidGameViewController: UIViewController, SCNSceneRendererDelegate, SC
     var gameScene: SCNScene!
     var cameraNode: SCNNode!
     var shipNode: SCNNode!
+    var springNode: SCNNode!
     
     var asteroidCreationTiming: Double = 0
-    var gameState: GameState!
+    var gameState: GameState = GameState.paused
     
-    
+    let horizontalBound: Float = 4 //was 7
+    let upperBound: Float = 6 //was 8
+    let lowerBound: Float = -6
+    let edgeWidth: Float = 3
     
     
     
@@ -57,6 +61,8 @@ class AsteroidGameViewController: UIViewController, SCNSceneRendererDelegate, SC
         initScene()
         initCamera()
         initShip()
+        
+        gameState = .playing
         
         objMotionControl.setDevicePitchOffset()
         
@@ -88,13 +94,15 @@ class AsteroidGameViewController: UIViewController, SCNSceneRendererDelegate, SC
         cameraNode = SCNNode()
         cameraNode.camera = SCNCamera()
         let cameraAngle: Float = 20
+        let cameraDistance: Float = 10
         cameraNode.eulerAngles = SCNVector3(x: -.pi*cameraAngle/180, y: 0, z: 0)
-        cameraNode.position = SCNVector3(x: 0, y: tan(.pi*cameraAngle/180)*10, z: 10)
+        cameraNode.position = SCNVector3(x: 0, y: tan(.pi*cameraAngle/180)*cameraDistance + 1, z: cameraDistance)
         //cameraNode.position = SCNVector3(x: 0, y: 0, z: 10)
         gameScene.rootNode.addChildNode(cameraNode)
     }
     
     func initShip() {
+        //set the ship
         let shipScene = SCNScene(named: "art.scnassets/retrorockett4k1t.dae")
         shipNode = shipScene?.rootNode
         shipNode.position = SCNVector3(x: 0, y: 0, z: 0)
@@ -102,35 +110,39 @@ class AsteroidGameViewController: UIViewController, SCNSceneRendererDelegate, SC
         shipNode.eulerAngles = SCNVector3(x: -(Float.pi/2), y: 0, z: 0)
         // setting the physicsbody of the ship
         shipNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
-        shipNode.physicsBody?.damping = 0.2
+        shipNode.physicsBody?.damping = 0.05
         shipNode.physicsBody?.categoryBitMask = CollisionMask.ship.rawValue
         shipNode.physicsBody?.contactTestBitMask = CollisionMask.asteroid.rawValue
         shipNode.name = "ship"
         gameScene.rootNode.addChildNode(shipNode)
-        gameState = GameState.playing
+        
+        
         
         //for debugging below
         print(gameState)
     }
     
     
+    
+    
     func createAsteroid() {
         // create the SCNGeometry
-        let asteroidGeometry = SCNCapsule(capRadius: 3, height: 7)
+        let asteroidGeometry = SCNCapsule(capRadius: 2.5, height: 6)
         // create the SCNNode with SCNGeometry
         let asteroidNode = SCNNode(geometry: asteroidGeometry)
         
         //setting the asteroid spawn position
-        let randomAsteroidPositionX = Float((drand48()-0.5)*6)
-        let randomAsteroidPositionY = Float((drand48()-0.5)*6)
+        let asteroidSpawnRange = 10.0
+        let randomAsteroidPositionX = Float((drand48()-0.5)*asteroidSpawnRange)
+        let randomAsteroidPositionY = Float((drand48()-0.5)*asteroidSpawnRange)
         asteroidNode.position = SCNVector3(x: randomAsteroidPositionX, y: randomAsteroidPositionY, z: -50)
         
         // put the asteroid into the rootNode
-        gameScene.rootNode.addChildNode(asteroidNode)
-        
         asteroidNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
         asteroidNode.physicsBody?.categoryBitMask = CollisionMask.asteroid.rawValue
         asteroidNode.physicsBody?.damping = 0
+        gameScene.rootNode.addChildNode(asteroidNode)
+        
         //apllying forces and torques
         let asteroidInitialForce = SCNVector3(x: 0, y: 0, z: 10)
         asteroidNode.physicsBody?.applyForce(asteroidInitialForce, asImpulse: true)
@@ -158,24 +170,59 @@ class AsteroidGameViewController: UIViewController, SCNSceneRendererDelegate, SC
     
     
     
-    // MARK: SCNSceneRendererDeligate functions
+    // MARK: SCNSceneRendererDeligate Functions
     
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
         
-        //
-        devicePitchOffset = objMotionControl.devicePitchOffset
+        
+        
+        // following part are controls for the ship, need to able to switch between iphoneX and others
         
         //read device motion (attitude)
         objMotionControl.updateDeviceMotionData()
         
         //set control of the ship
-        shipNode.physicsBody?.applyForce(SCNVector3(x: Float(objMotionControl.roll*3.0), y: Float(-((objMotionControl.pitch)-objMotionControl.devicePitchOffset)*3.0), z: 0), asImpulse: false)
+        
+        
+        var horizontalCentralForce: Float = 0
+        var verticalCentralForce: Float = 0
+        if shipNode.presentation.position.x > (horizontalBound - edgeWidth) && (shipNode.physicsBody?.velocity.x)! > Float(0) {
+            horizontalCentralForce = (shipNode.presentation.position.x - (horizontalBound - edgeWidth)) / edgeWidth * -3
+        }
+        else if shipNode.presentation.position.x < (-horizontalBound + edgeWidth) && (shipNode.physicsBody?.velocity.x)! < Float(0)  {
+            horizontalCentralForce = (shipNode.presentation.position.x - (-horizontalBound + edgeWidth)) / edgeWidth * -3
+        }
+        else {
+            horizontalCentralForce = 0
+        }
+        if shipNode.presentation.position.y > (upperBound - edgeWidth) && (shipNode.physicsBody?.velocity.y)! > Float(0) {
+            verticalCentralForce = (shipNode.presentation.position.y - (upperBound - edgeWidth)) / edgeWidth * -5
+        }
+        else if shipNode.presentation.position.y < (lowerBound + edgeWidth) && (shipNode.physicsBody?.velocity.y)! < Float(0) {
+            verticalCentralForce = (shipNode.presentation.position.y - (lowerBound + edgeWidth)) / edgeWidth * -5
+        } else {
+            verticalCentralForce = 0
+        }
+        
+        let shipControlForce = SCNVector3(x: Float(objMotionControl.roll*6.0) + horizontalCentralForce, y: Float(-((objMotionControl.pitch)-objMotionControl.devicePitchOffset)*10.0) + verticalCentralForce, z: 0)
+        
+        shipNode.physicsBody?.applyForce(shipControlForce, asImpulse: false)
 
 
+        //now i just reset the ship after the ship died, should be removed after the interface is set
         if(gameState == GameState.dead) {
             gameState = GameState.playing
             initShip()
         }
+        
+        //dead if ship is too far away
+        
+        if abs(shipNode.presentation.position.x) > horizontalBound || shipNode.presentation.position.y > upperBound || shipNode.presentation.position.y < lowerBound {
+            shipNode.removeFromParentNode()
+            gameState = .dead
+        }
+    
+        
         
         //creating asteroids and cleaning up asteroids
         if time > asteroidCreationTiming {
@@ -186,7 +233,9 @@ class AsteroidGameViewController: UIViewController, SCNSceneRendererDelegate, SC
     }
     
     
-    // MARK: SCNPhysicsContactDelegate functions
+    
+    
+    // MARK: SCNPhysicsContactDelegate Functions
     func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
         gameScene.rootNode.childNode(withName: "ship", recursively: false)?.removeFromParentNode()
         gameState = GameState.dead
@@ -199,7 +248,7 @@ class AsteroidGameViewController: UIViewController, SCNSceneRendererDelegate, SC
     
     
     
-    
+    // MARK: ViewController Settings
     override var shouldAutorotate: Bool {
         return false
     }
